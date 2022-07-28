a5d47c4f937e98d00120a4d4b4a590cdbb461369
jagad chandana <jagadchandana61@gmail.com>
2022-07-28 11:24:49 +0530

    Public with Member

---
+++ resources/views/public-area/libraries/scripts.blade.php
@@ -0,0 +1,10 @@
+ <!-- Vendor JS Files -->
+ <script src="{{ asset('public-area/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+ <script src="{{ asset('public-area/vendor/aos/aos.js') }}"></script>
+ <script src="{{ asset('public-area/vendor/glightbox/js/glightbox.min.js') }}"></script>
+ <script src="{{ asset('public-area/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+ <script src="{{ asset('public-area/vendor/swiper/swiper-bundle.min.js') }}"></script>
+ <script src="{{ asset('public-area/vendor/php-email-form/validate.js') }}"></script>
+
+ <!-- Template Main JS File -->
+ <script src="{{ asset('public-area/js/main.js') }}"></script>
